241f203a5ac0c8def7d15ffe93cdaa977778b299
Manoj Kulkarni <manoj.kulkarni.3011@gmail.com>
2019-04-26 23:52:04 +0530

    Empty View Page Is Added

--- CollectionViewApp/CollectionViewApp/ViewModels/EmptyViewModel.cs
+++ CollectionViewApp/CollectionViewApp/ViewModels/EmptyViewModel.cs
@@ -1,10 +1,22 @@
 ﻿using CollectionViewApp.Models;
+using CollectionViewApp.Services;
 using MvvmHelpers;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Xamarin.Forms;
 
 namespace CollectionViewApp.ViewModels
 {
     public class EmptyViewModel : BaseViewModel
     {
+        #region Fields  
+
+        private readonly MonkeyService _monkeyService;
+
+        #endregion
+
         #region Public Properties
 
         private ObservableRangeCollection<Monkey> _monkeys;
@@ -16,11 +28,50 @@ namespace CollectionViewApp.ViewModels
 
         #endregion
 
+        #region Commands
+
+        private ICommand _loadDataCommand;
+        public ICommand LoadDataCommand => _loadDataCommand ?? (_loadDataCommand = new Command(async () => await GetMonkeysListAsync()));
+
+        #endregion
+
         #region Constructor
 
         public EmptyViewModel()
         {
             Monkeys = new ObservableRangeCollection<Monkey>();
+            _monkeyService = new MonkeyService();
+        }
+
+        #endregion
+
+        #region Methods
+
+        private async Task GetMonkeysListAsync()
+        {
+            if (IsBusy)
+            {
+                return;
+            }
+
+            IsBusy = true;
+
+            try
+            {
+                var monkeys = await _monkeyService.GetAllMonkeysAsync();
+                if (monkeys != null && monkeys.Any())
+                {
+                    Monkeys.AddRange(monkeys);
+                }
+            }
+            catch (Exception ex)
+            {
+                System.Diagnostics.Debug.WriteLine(ex.Message);
+            }
+            finally
+            {
+                IsBusy = false;
+            }
         }
 
         #endregion
